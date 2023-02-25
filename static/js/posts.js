463a0def081d057d8c71bdf1921f9302dc1f8a68
davon <davon@gmail.com>
2023-02-25 12:38:56 -0500

    first commit

---
+++ static/js/posts.js
@@ -0,0 +1,9 @@
+/////////////////////////////
+//javascript for posts page
+/////////////////////////////
+
+$(function(){
+    $('.js-menu-icon').click(function(){
+        $(this).next().toggle();
+    })
+})
